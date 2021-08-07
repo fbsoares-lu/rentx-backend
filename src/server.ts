19af52b133352207f0cef6971bac3345dc8f893c
fbsoares-lu <lsoareshfb@gmail.com>
2021-08-07 12:17:01 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ name });
+});
+
+app.listen(3333, () => console.log("Server is Running!"));
